8b46f2cd9959c64f0cf7391dfbff089800cee47d
silverdruid <zlatantemi@gmail.com>
2022-09-23 09:09:40 +0100

    js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 let btn = document.getElementById('new-quote')
-let quote = document.querySelector('quote')
+let quote = document.querySelector('.quote')
+let person = document.querySelector('.person')
+
+window.addEventListener("load", getQuote())
+btn.addEventListener("click", getQuote())
+
+  function getQuote() {
+    const url = "https://api.quotable.io/random"
+
+    fetch(url).then((data) = data.json()).then((item) => {
+        quote.innerText = item.content
+        person.innerText = item.author
+    })
+}
+
