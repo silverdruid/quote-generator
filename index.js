081f2e71540d5c0428a8b437877c11fc79364d69
silverdruid <zlatantemi@gmail.com>
2022-09-23 12:29:17 +0100

    updated index.js

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ let btn = document.getElementById('new-quote')
 let quote = document.querySelector('.quote')
 let person = document.querySelector('.person')
 
-window.addEventListener("load", getQuote())
-btn.addEventListener("click", getQuote())
+window.addEventListener("load", getQuote)
+btn.addEventListener("click", getQuote)
 
-  function getQuote() {
-    const url = "https://api.quotable.io/random"
+const url = "https://api.quotable.io/random"
 
-    fetch(url).then((data) = data.json()).then((item) => {
-        quote.innerText = item.content
-        person.innerText = item.author
-    })
+function getQuote() {
+      
+    // fetch(url)
+    //     .then((data) = data.json()).then((item) => {
+    //     quote.innerText = item.content
+    //     person.innerText = item.author
+    // })
+
+    fetch(url)
+        .then((data) => {
+            return data.json()
+        })
+        .then((data) => {
+            quote.innerText = data.content
+            person.innerText = "- " + data.author
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
 
+setInterval(getQuote(), 10000)
